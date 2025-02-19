7ca39b348ec5b50ee31290b5da58c884809211ee
KristinFrida <kfs14@hi.is>
2025-02-19 19:01:46 +0000

    Aðeins fallegra

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,6 @@ export const router = express.Router();
 
 router.get('/', async (req, res) => {
   const result = await getDatabase()?.query('SELECT * FROM categories');
-
   const categories = result?.rows ?? [];
 
   console.log(categories);
@@ -20,8 +19,12 @@ router.get('/spurningar/:category', (req, res) => {
   res.render('category', { title });
 });
 
-router.get('/form', (req, res) => {
-  res.render('form', { title: 'Búa til flokk' });
+router.get('/form', async (req, res) => {
+  // Sækjum flokka til að birta í dropdown (ef til)
+  const result = await getDatabase()?.query('SELECT * FROM categories');
+  const categories = result?.rows ?? [];
+
+  res.render('form', { title: 'Búa til spurningu', categories });
 });
 
 router.post('/form', async (req, res) => {
@@ -29,11 +32,7 @@ router.post('/form', async (req, res) => {
 
   console.log(name);
 
-  // Hér þarf að setja upp validation, hvað ef name er tómt? hvað ef það er allt handritið að BEE MOVIE?
-  // Hvað ef það er SQL INJECTION? HVAÐ EF ÞAÐ ER EITTHVAÐ ANNAÐ HRÆÐILEGT?!?!?!?!?!
-  // TODO VALIDATION OG HUGA AÐ ÖRYGGI
-
-  // Ef validation klikkar, senda skilaboð um það á notanda
+  // TODO: Bæta við validation og öryggisathugunum
 
   // Ef allt OK, búa til í gagnagrunn.
   const env = environment(process.env, logger);
