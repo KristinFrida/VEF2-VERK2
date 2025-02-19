5e06466b88dd9e0d81634d660479d0db7d9e9e29
KristinFrida <kfs14@hi.is>
2025-02-19 23:12:38 +0000

    ég græt meira

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,16 +4,16 @@ CREATE TABLE IF NOT EXISTS public.categories (
   created timestamp with time zone not null default current_timestamp
 );
 
-CREATE TABLE IF NOT EXISTS spurningar (
+CREATE TABLE IF NOT EXISTS public.spurningar (
   id SERIAL PRIMARY KEY,
   spurning TEXT NOT NULL,
-  category_id INTEGER NOT NULL REFERENCES categories(id),
+  category_id INTEGER NOT NULL REFERENCES public.categories(id),
   created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS svor (
+CREATE TABLE IF NOT EXISTS public.svor (
   id SERIAL PRIMARY KEY,
   svor_text TEXT NOT NULL,
-  spurning_id INTEGER NOT NULL REFERENCES spurningar(id),
+  spurning_id INTEGER NOT NULL REFERENCES public.spurningar(id),
   is_correct BOOLEAN NOT NULL DEFAULT FALSE
 );
